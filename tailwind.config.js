33ce34530ddc1fb9af80dc44db7123925dde5d5e
pabitra <89703515+appu900@users.noreply.github.com>
2023-12-23 03:39:19 +0530

    backend connection done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./index.html"],
+  purge: ["./index.html","./registration.html"],
   theme: {
     extend: {
       fontFamily: {
